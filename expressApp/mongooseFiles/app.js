0e85c09f3b991cb8306a815411ad9eb703cd5d36
narek <narek_abrahamyan@epam.com>
2019-03-19 16:33:28 +0400

    7tn

---
+++ expressApp/mongooseFiles/app.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+// import router
+const { citiesRoutes, usersRoutes, productsRoutes } = require('./routes');
+
+// connect to database
+const URI = 'mongodb+srv://narek:narek@cluster0-6nwae.mongodb.net/nodeHom';
+mongoose.connect(URI, { useNewUrlParser: true });
+const db = mongoose.connection;
+
+
+
+// check for database errors
+db.on('error', error => console.log(error));
+// check connection
+db.once('open', () => console.log('Connected to MongoDB'));
+
+// init express app
+const app = express();
+
+// Body Parser middleware application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.end('hello express');
+});
+
+app.get('/api', (req, res) => res.status(200).send({
+  message: 'Welcome to the API!',
+}));
+
+// Routes
+app.use('/api/cities', citiesRoutes);
+app.use('/api/users', usersRoutes);
+app.use('/api/products', productsRoutes);
+
+app.listen(8082, () => console.log('Mongoose app Listening on port 8082'));
+
+module.exports = app;
